464839fa86f8bad60a352c4c26c8480074904576
hvsharma63 <hvsharma63@gmail.com>
2019-10-29 20:10:22 +0530

    [WIP] Sidenav Updated

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent implements OnInit {
+
+  @Output() sideNavClose = new EventEmitter<void>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSideNavClose(){
+    this.sideNavClose.emit();
+  }
+}
